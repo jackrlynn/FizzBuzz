02495b700bdbcd50e23c0b7357ef7a79f49adf45
jackrlynn <jacklynn@uiowa.edu>
2022-01-10 16:09:16 -0600

    Added game ending functionality to FizzBuzz game section

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,5 +1,6 @@
 // Hidden variable that keeps current track of where the player is
 let currVal = 1;
+let score = 0;
 
 // Runs a check of the current round upon button click
 //   choice: the choice the user made
@@ -23,16 +24,52 @@ function checkRound(choice) {
         // Debugging logging
         console.log("Correct choice made");
 
+        // Update score: 10 for num, 50 for Fizz or Buzz, and 100 for FizzBuzz
+        if (correct === "Fizz" || correct === "Buzz") {
+            score += 50;
+        } else if (correct === "FizzBuzz") {
+            score += 100;
+        } else {
+            score += 10;
+        }
+
+        document.getElementById("score").innerHTML = "Score: " + score;
+
     } else { // Incorrect choice made
 
-        // Reset currVal
-        currVal = 1;
-
-        // Reset number button
-        document.getElementById("num").innerHTML = 1;
-
-        // Debugging loggin
+        // Debugging logging
         console.log("Incorrect choice made");
+
+        // Inactivate play buttons
+        document.getElementById("fizz").disabled = true;
+        document.getElementById("buzz").disabled = true;
+        document.getElementById("fizzbuzz").disabled = true;
+        document.getElementById("num").disabled = true;
+
+        // Activate restart button
+        document.getElementById("restart").disabled = false;
     }
 
+}
+
+function restart() {
+
+    // Reset currVal
+    currVal = 1;
+
+    // Reset number button
+    document.getElementById("num").innerHTML = 1;
+
+    // Reset score
+    score = 0;
+    document.getElementById("score").innerHTML = "Score: " + score;
+
+    // Enable play buttons
+    document.getElementById("fizz").disabled = false;
+    document.getElementById("buzz").disabled = false;
+    document.getElementById("fizzbuzz").disabled = false;
+    document.getElementById("num").disabled = false;
+
+    // Disable restart button
+    document.getElementById("restart").disabled = true;
 }
